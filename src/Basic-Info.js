8293cb5923a9bf5eb71c2ba4dc4c63cffb1d3b9f
jimmybnguyen <jimmyn9@uw.edu>
2016-12-04 10:40:29 -0800

    render pokemon types icon

--- src/Basic-Info.js
+++ src/Basic-Info.js
@@ -5,8 +5,17 @@ class BasicInfo extends React.Component {
             return (
                 <div> 
                     <h2>NO. {this.props.id} | {this.props.name}</h2>
+                
                     <img alt="pokemon sprite" src={this.props.spritePath} />
+                
+                    {
+                        this.props.types.map((types) => (
+                         <img key={this.props.id + types.type.name} alt="pokemon type" src={"https://veekun.com/dex/media/types/en/" + types.type.name + ".png"} />
+                        ))
+                    }
+
                     <p>Height: {this.props.height} | Weight: {this.props.weight}</p>
+                    
                     <audio autoPlay controls src={"https://veekun.com/dex/media/pokemon/cries/" + this.props.id + ".ogg"}></audio>
                 </div>
             );
