1f6d6513a2e8930bd82e68beef0b1041ced12b51
jimmybnguyen <jimmyn9@uw.edu>
2016-12-04 12:31:11 -0800

    render pokemon stats

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Heading from './Heading.js';
 import SearchForm from './Search-Form.js';
 import BasicInfo from './Basic-Info.js';
+import Stats from './Stats.js';
 
 var BEGINNING_URL = 'http://pokeapi.co/api/v2/pokemon/';
 
@@ -15,29 +16,37 @@ class App extends Component {
         };
     }
     
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-            <Heading />
-        </div>
-            <SearchForm 
-                onSearch={(pokemon) => this.searchPokemon(pokemon)}
-            />
-            {
-                this.state.name ? (
-                    <BasicInfo
-                        id={this.state.id} 
-                        name={this.state.name}
-                        spritePath={this.state.spritePath}
-                        types={this.state.types}
-                        height={this.state.height}
-                        weight={this.state.weight}
-                    />
-                ) : null
-            }
-      </div>
-    );
+    render() {
+        return (
+          <div className="App">
+            <div className="App-header">
+                <Heading />
+            </div>
+                <SearchForm 
+                    onSearch={(pokemon) => this.searchPokemon(pokemon)}
+                />
+                {
+                    this.state.name ? (
+                        <BasicInfo
+                            id={this.state.id} 
+                            name={this.state.name}
+                            spritePath={this.state.spritePath}
+                            types={this.state.types}
+                            height={this.state.height}
+                            weight={this.state.weight}
+                        />
+                    ) : null
+                }
+                {
+                    this.state.stats ? (
+                        <Stats
+                            id={this.state.id}
+                            stats={this.state.stats}
+                        />
+                    ) : null
+                }
+          </div>
+        );
   }
   
     searchPokemon(pokemon) {
@@ -51,19 +60,42 @@ class App extends Component {
             console.log(json);
             var id = json.id;
             var name = this.capitalizeFirstLetter(json.forms[0].name);
-            var spirtePath = json.sprites.front_default;
+            var spritePath = json.sprites.front_default;
             var types = json.types;
             var height = (json.height / 10) + "m";
             var weight = (json.weight / 10) + "kg";
+            var stats = this.calculateTotalStats(json.stats);
             this.setState({
                 id: id,
                 name: name,
-                spritePath: spirtePath,
+                spritePath: spritePath,
                 types: types,
                 height: height,
-                weight: weight
+                weight: weight,
+                stats: stats
             });
         });
+    }
+
+    // Returns an array with all of the stats from the api, plus value of all the stats
+    calculateTotalStats(stats) {
+        var totalValue = 0;
+        
+        // Calculates the total stat value of the pokemon
+        for (var i=0; i < stats.length; i++) {
+            totalValue += stats[i].base_stat;
+        }
+        
+        // Creates new object to be added to stats array
+        var totalStat = {
+            "base_stat": totalValue,
+            "effort": 0,
+            "stat": {"name": "total" }
+        }
+        
+        // Adds the total stat object to the start of the stats array
+        stats.unshift(totalStat);
+        return stats;
     }
 
     //http://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
