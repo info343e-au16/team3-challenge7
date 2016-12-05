9b32f5e5dae899e130efdfb73590923fd7215a61
jimmybnguyen <jimmyn9@uw.edu>
2016-12-04 23:56:35 -0800

    add raised button to search

--- src/Search-Form.js
+++ src/Search-Form.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class SearchForm extends React.Component {
     render() {
             return (
                 <form onSubmit={(e) => this.onSearch(e)}>  
-                    <div>
-                        <input type="text" ref="query" placeholder="Search for Pokemon"/>
-                        <div>
-                            <button type="submit">Go!</button>
+                    <div className="col-lg-6">
+                        <div className="input-group">
+                            <input type="text" ref="query" className="form-control" placeholder="Search for Pokemon"/>
+                            <div className="input-group-btn">
+                                <RaisedButton label="Go!" type="submit" />
+                            </div>
                         </div>
                     </div>
                 </form>
