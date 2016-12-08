b3737e09ff430887b572c9606c460a712f01ace7
jimmybnguyen <jimmyn9@uw.edu>
2016-12-07 19:07:03 -0800

    change stats to table

--- src/Stats.js
+++ src/Stats.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import './Stats.css';
+import { Table, Row, Col } from 'react-materialize';
 
 class Stats extends React.Component {
     render() {
             return (
                 <div className="container"> 
-                    <h2>Stats</h2>
-                    <table>
+                    <h2>Base Stats</h2>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Stat</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
                         <tbody>
                             {
                             this.props.stats.map((stats) => (
                                 <tr key={stats.stat.name}>
                                     <td>{stats.stat.name}</td>
-                                    <td className="right">{stats.base_stat}</td>
+                                    <td>{stats.base_stat}</td>
                                 </tr>
                             ))
                             }
                         </tbody>
-                    </table>
+                    </Table>
                 </div>
             );
     }
